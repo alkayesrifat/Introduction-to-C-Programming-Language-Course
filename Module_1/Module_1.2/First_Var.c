11abefc62ca712a65a7c0300ee353aa495f8b920
alkayesrifat <157808289+alkayesrifat@users.noreply.github.com>
2025-02-25 11:05:09 +0600

    Add initial files and content for Module 1.2: Variables and Data Types

---
+++ Module_1/Module_1.2/First_Var.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+
+    int dadarage = 50;
+
+    int dadarboyos ;
+
+    dadarboyos = 60 ;
+
+    char grade = 'A';
+
+    char grade2 = 'b';
+
+    char graded ;
+
+    graded = 'F';
+
+
+
+    return 0;
+}
---
+++ Module_1/Module_1.2/var_print.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+    int dadarboyos = 60;
+
+
+
+    printf("Amar dadar %d boyos = %d\t",dadarboyos,dadarboyos);
+    
+    char grade = 'A';
+
+    
+
+    printf("My Grade is = %c",grade);
+
+
+
+    return 0;
+}
