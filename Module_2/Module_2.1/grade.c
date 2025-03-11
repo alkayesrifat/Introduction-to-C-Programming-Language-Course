ad54621aa17cdc1d278988a7ec858d8a73d8e259
alkayesrifat <157808289+alkayesrifat@users.noreply.github.com>
2025-03-11 10:36:34 +0600

    Add grading and conditional logic programs in C with example inputs and outputs

---
+++ Module_2/Module_2.1/grade.c
@@ -0,0 +1,39 @@
+/**
+ * All Praise to Allah
+ * ---------------------
+ * Author: Al Kayes Rifat
+ * Created: 2025-03-11   10:25:41
+ * Source: 
+ */
+
+#include <stdio.h>
+
+int main() {
+
+    int n ;
+    scanf("%d",&n);
+
+    if (n >= 80 && n<=90 )
+    {
+        printf("A+");
+    }
+    if (n>=70 && n < 80)
+    {
+        printf("A");
+    }
+    else
+    {
+        printf("Tumi a+ ba a paw nai");
+    }
+    
+    //a-
+    //b
+    // fail
+
+    
+
+
+
+    
+    return 0;
+}
---
+++ Module_2/Module_2.1/if_else.c
@@ -0,0 +1,60 @@
+/**
+ * All Praise to Allah
+ * ---------------------
+ * Author: Al Kayes Rifat
+ * Created: 2025-03-11   10:16:29
+ * Source: 
+ */
+
+#include <stdio.h>
+
+int main() {
+
+    int dimer_dam ;
+    scanf("%d",&dimer_dam);
+
+    if(dimer_dam <= 40){
+        printf("4 ta dim kine ano");
+    }
+    else
+    {
+        printf("3 ta dim kine ano");
+        
+    }
+
+    /*
+
+    if( condition )
+
+    {
+
+        instruction or code
+    
+    }
+
+    else 
+    {
+        instruction or code
+    }
+    
+    
+    
+    
+    
+    */
+
+    if (condition)
+    {
+        /* code */
+    }
+    else
+    {
+        /* code */
+    }
+    
+    
+    
+
+    
+    return 0;
+}
